4da6593533fecfb2651543fe2024b83415cb8fb9
Alan Gardner <urfolomeus@gmail.com>
2018-08-08 12:54:59 +0100

    Can identify lowercase isograms

--- src/isogram.c
+++ src/isogram.c
@@ -1,4 +1,11 @@
 #include "isogram.h"
+#include <string.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b)
+{
+  return *(const char *)a - *(const char *)b;
+}
 
 bool is_isogram(const char phrase[])
 {
@@ -7,5 +14,28 @@ bool is_isogram(const char phrase[])
     return true;
   }
 
-  return false;
+  char *sortable_phrase = malloc(strlen(phrase) + 1);
+
+  int sortable_phrase_index = 0;
+
+  for (int i = 0; phrase[i]; i++)
+  {
+    sortable_phrase[sortable_phrase_index] = phrase[i];
+    ++sortable_phrase_index;
+  }
+
+  qsort(sortable_phrase, strlen(sortable_phrase), 1, compare);
+
+  for (int j = 1; sortable_phrase[j]; j++)
+  {
+    if (sortable_phrase[j] == sortable_phrase[j - 1])
+    {
+      free(sortable_phrase);
+      return false;
+    }
+  }
+
+  free(sortable_phrase);
+
+  return true;
 }
